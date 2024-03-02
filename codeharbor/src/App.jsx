cf452aa28335f1311d8ab64c64f4d90530f74948
lucasguimaraes2005 <lucaagui123@gmail.com>
2024-03-02 13:37:59 -0300

    feat: iniciando

---
+++ codeharbor/src/App.jsx
@@ -0,0 +1,13 @@
+import { HomePage } from './pages/HomePage.jsx'
+import "./styles/index.scss"
+
+function App() {
+
+  return (
+    <>
+      <HomePage />
+    </>
+  )
+}
+
+export default App
---
+++ codeharbor/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+export const Header = () => {
+    return (
+        <>
+        <header className="header">
+            <div className="headerContainer">
+                <img src="https://cdn.discordapp.com/attachments/1200202645726580758/1213169364430487643/OIG4.png?ex=65f47f04&is=65e20a04&hm=adae2531a2c0b178adc8a75599429b4ae10e915f0bd96b6db5fe3a8bcbe3e329&" alt="Logo" />
+
+                <nav>
+                    <a href="#sobre-nos">Sobre nós</a>
+                    <a href="#contato">Contato</a>
+                    <a href="#servicos">Serviços</a>
+                </nav>
+            </div>
+        </header>
+        </>
+    )
+}
