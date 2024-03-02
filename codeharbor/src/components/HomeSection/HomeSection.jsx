14e462c7bb47fa056a004a73a01763fdfbd09b10
lucasguimaraes2005 <lucaagui123@gmail.com>
2024-03-02 19:33:42 -0300

    adicionado link para discord no homesection

--- codeharbor/src/components/HomeSection/HomeSection.jsx
+++ codeharbor/src/components/HomeSection/HomeSection.jsx
@@ -7,7 +7,7 @@ export const HomeSection = () => {
                     Bem vindo ao seu porto seguro no desenvolvimento WEB.
                 </h1>
                 <p>
-                    Confiáveis e solicitos ao seu pedido, com os preços mais acessíveis do mercado. Conheça mais sobre a gente!
+                    Confiáveis e solicitos ao seu pedido, com os preços mais acessíveis do mercado. <a href="https://discord.gg/yRd4UgMEwn">Clique aqui e conheça mais sobre a gente!</a>
                 </p>
             </div>
             <img src="https://cdn.discordapp.com/attachments/1200202645726580758/1213591632579141662/code-removebg-preview.png?ex=65f60848&is=65e39348&hm=91ba8c2a4a91a0c9ba9c395277f71e563dd4c61313966aa79332c86e69e44f02&" alt="CodeHarbor" />
