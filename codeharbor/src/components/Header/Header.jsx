902b89deb556013522b30ebec17251995edde950
lucasguimaraes2005 <lucaagui123@gmail.com>
2024-03-18 13:40:34 -0300

    feat: mudanças nas importações de img

--- codeharbor/src/components/Header/Header.jsx
+++ codeharbor/src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
+import headerImage from "../../assets/header.png"
+
 export const Header = () => {
     return (
         <>
         <header className="header">
             <div className="headerContainer">
-                <img src="https://cdn.discordapp.com/attachments/1200202645726580758/1213553726162342018/xzcmWG-UQ4CHlFZbzulhTQ-removebg-preview.png?ex=65f5e4fb&is=65e36ffb&hm=3fb292ef140b911fcb2bfbcdd3a530d59674d9a3c88fe9ae1d18dc19b1fe56e4&" alt="Logo" />
+                <img src={headerImage} alt="Logo" />
 
                 <nav>
                     <a href="#sobre-nos">Sobre nós</a>
